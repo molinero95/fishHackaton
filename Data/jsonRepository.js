14c05a3084e1e6dff3b1086a7fa151e30020590e
Andrei Ionut Vaduva <andi_vaduva@yahoo.es>
2018-02-10 13:24:05 +0100

    data

--- Data/jsonRepository.js
+++ Data/jsonRepository.js
@@ -1,14 +1,28 @@
 "use strict";
 
-class jsonRepositoy{
+function jsonRepository() {
 
-
-
-
-
+    this.getFishingGrounds = function (callback) {
+        $.ajax({
+            url: "http://localhost:8000/fishackaton/JSON/fishingGrounds.json",
+            type: 'GET',
+            dataType: 'json',
+            cors: true,
+            contentType: 'application/json',
+            secure: true,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+            beforeSend: function (xhr) {
+                // xhr.setRequestHeader ("Authorization", "Basic " + btoa(""));
+            },
+            success: function (data) {
+                alert(data);
+                callback(data);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(thrownError);
+            }
+        });
+    }
 };
-
-
-module.exports = {
-    jsonRepositoy: jsonRepositoy
-}
