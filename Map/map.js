53ef621fbb93524d51bb5e9af7b8e45030f4ce09
molinero95 <jaimolas95@gmail.com>
2018-02-10 17:27:10 +0100

    actualizo

--- Map/map.js
+++ Map/map.js
@@ -13,30 +13,29 @@ let addListenersOnPolygon = function(polygon) {
     });  
 }
 
-function generatePolygon(path){
+function generatePolygon(path, color){
+    console.log(color);
     return new google.maps.Polygon({
         path: path,
         geodesic: true,
-        strokeColor: '#FF0000',
+        strokeColor: color,
         strokeOpacity: 1.0,
         strokeWeight: 2
     });
 }
 
-//TODO poner las coordenadas de fichero
-//Por ahora demo de como será
+
 function generatePolygons(data, map){
-    console.log(data);
-    //console.log(data);
-    /*let ubications = Object.keys(data);
-    for(let i = 0; i < ubications.length; i++){
-        //console.log(ubications);
-        let positions = Object.keys(data[ubications[i]]);
-        for(let j = 0; j < positions.length; j++){
-            console.log(data[ubications[i]][positions[j]]);
-        }
-        //console.log(data[ubications[i]]);
+    data.forEach(element => {
+        let coordsStr = element.coords;
+        let coords = [];
+        coordsStr.forEach(e => {
+            coords.push({lat: Number(e.lat), lng: Number(e.lng)});
+        });
+        let polygon = generatePolygon(coords, element.color);
+        polygon.setMap(map);
+        addListenersOnPolygon(polygon);
         
-    }*/
+    });
 }
 
